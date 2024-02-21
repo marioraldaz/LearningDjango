634035decc97f6859c05cea1fe539573ea9560cd
marioraldaz <marioraldaz@gmail.com>
2024-02-21 20:36:29 +0100

    tasklist comp

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -3,13 +3,23 @@ import { useEffect, useState } from "react";
 import { getAllTasks } from "../api/tasks.api";
 
 export function TaskList() {
+  const [tasks, setTasks] = useState([]);
   useEffect(() => {
     async function loadTasks() {
       const res = await getAllTasks();
-      console.log(res.data);
+      setTasks(res.data);
     }
     loadTasks();
   }, []);
 
-  return <div>TaskList</div>;
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.title}>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
