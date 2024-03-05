416ce9dc7cefe6a873343045fd3a861e53e405e8
marioraldaz <marioraldaz@gmail.com>
2024-03-05 20:36:01 +0100

    fixxed error message in register

--- client/src/pages/Profile/Register.jsx
+++ client/src/pages/Profile/Register.jsx
@@ -14,16 +14,16 @@ export function Register() {
     const errors = Object.entries(error.response.data);
     let errorMessage = "";
     errors.forEach((error) => {
-      errorMessage += `${error[0]} &nbsp: ${error[1]}\n`;
+      errorMessage += `${error[0]}: ${error[1]}\n`;
     });
-    setError(errors[0]); 
+    setError(errorMessage); 
   }
   };
     return (
     <div className="flex items-center justify-center mt-[40px]">
        <form onSubmit={handleSubmit} className="w-[500px] bg-neutral-800 p-[50px] h-full rounded-2xl text-black" >
         <h1 className="text-4xl text-center text-white"> Register </h1>
-        {error && <h1 className="text-red-600">{error.toString()}</h1>}
+        {error && <h1 className="text-red-600 mt-8">{error}</h1>}
           <div className="mt-10 flex justify-center items-center flex-col">
             <label htmlFor="username" className="block  text-green-600 font-semibold">Username:</label>
             <input
--- profiles/user.py
+++ profiles/user.py
@@ -1,10 +1,9 @@
 from django.db import models
-# Create your models here.
-
+from django.core.validators import MinLengthValidator
 
 class User(models.Model):
-    username = models.CharField(max_length=25)
-    password = models.CharField(max_length=25)
+    username = models.CharField(max_length=25, validators=[MinLengthValidator(6)])
+    password = models.CharField(max_length=25, validators=[MinLengthValidator(6)])
     gender = models.CharField(max_length=10, choices=(('Male', 'Male'), ('Female', 'Female')))
     email = models.EmailField(max_length=50)
     weight = models.IntegerField(default=80)
