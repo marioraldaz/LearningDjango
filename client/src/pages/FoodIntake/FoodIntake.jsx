eb23bb38ccf27bbce7078cf202371a9c3c07454d
marioraldaz <marioraldaz@gmail.com>
2024-05-11 19:15:14 +0200

    fixxed style for small screens

--- client/src/pages/FoodIntake/FoodIntake.jsx
+++ client/src/pages/FoodIntake/FoodIntake.jsx
@@ -29,15 +29,15 @@ export function FoodIntake() {
   }
 
   return (
-    <section className="p-4 grid 2xl:grid-cols-3 xl:grid-cols-2 bg-black grid-cols-1 mt-[50px] justify-center">
-      <div className="order-2 2xl:order1 h-[700px] w-[430px] xl:ml-[200px] 2xl:ml-0 flex flex-col overflow-y-auto items-center bg-neutral-700 p-4 rounded-lg">
+    <section className="p-4 grid 2xl:grid-cols-3 xl:grid-cols-2 bg-black grid-cols-1 mt-[50px] justify-center items-center">
+      <div className="order-2 2xl:order1 h-[700px] w-[430px] xl:ml-[200px] 2xl:ml-0 flex flex-col overflow-y-auto items-center bg-neutral-700 p-4 rounded-lg ml-auto mr-auto mt-4">
         <h3 className="text-2xl mb-4 ">Recently Seen Recipes</h3>
         <CardsList products={persistRecipes} />
       </div>
       <div className="order-1 2xl:order-2">
         <IntakeForm addFoodIntake={addFoodIntake} recipes={[recipeToAdd]} />
       </div>
-      <div className="order-3 2xl:order-3 h-[700px] w-[430px] flex flex-col overflow-y-auto items-center bg-neutral-700 p-4 rounded-lg ml-auto">
+      <div className="order-3 2xl:order-3 h-[700px] w-[430px] flex flex-col overflow-y-auto items-center bg-neutral-700 p-4 rounded-lg ml-auto mr-auto mt-4">
         <h3 className="">My Saved Recipes</h3>
         <CardsList products={savedRecipes} />
       </div>
